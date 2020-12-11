d8aaa412d4b695fc254ac4776298eaf63559c941
cklasicki <cklasicki@gmail.com>
2020-12-11 23:26:42 +0100

    duplicate completed

---
+++ tasks/arrays/dupllicate.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+def duplicate_number(arr: List):
+    """
+    :param - array containing numbers in the range [0, len(arr) - 2]
+    return - the number that is duplicate in the arr
+    """
+
+    for i in range(len(arr)-1):
+        if arr.count(i) > 1:
+            return i
+    return 0
+
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    output = duplicate_number(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr = [0, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 2, 3, 1, 4, 5, 3]
+solution = 3
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 4, 3, 2, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 5, 3, 2, 4]
+solution = 5
+
+test_case = [arr, solution]
+test_function(test_case)
