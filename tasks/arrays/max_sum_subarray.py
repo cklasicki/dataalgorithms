fa68869cd1841a154b1ef3e93c65134590b8324b
cklasicki <cklasicki@gmail.com>
2020-12-19 10:41:56 +0100

    max sum subarray completed

---
+++ tasks/arrays/max_sum_subarray.py
@@ -0,0 +1,44 @@
+def max_sum_subarray(arr):
+    """
+    :param - arr - input array
+    return - number - largest sum in contiguous subarry within arr
+    """
+    
+    current_sum = arr[0]
+    max_sum = arr[0]
+
+    for value in arr[1:]:
+        current_sum = max(current_sum +value, value)
+        max_sum = max(current_sum, max_sum)
+    
+    return max_sum
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    
+    output = max_sum_subarray(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+
+arr= [1, 2, 3, -4, 6]
+solution= 8 # sum of array
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [1, 2, -5, -4, 1, 6]
+solution = 7   # sum of last two elements
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
+solution = 18  # sum of subarray = [15, -13, 14, -1, 2, 1]
+
+test_case = [arr, solution]
+test_function(test_case)
